241dc63397003d6a1c28c14dab03271c283724af
Aptemond <sofartem5@gmail.com>
2023-06-15 00:13:06 +0500

    hw2

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,5 @@
 import os
+import json
 from googleapiclient.discovery import build
 
 api_key: str = os.getenv('YT_API_KEY')
@@ -6,11 +7,47 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 class Channel:
     """Класс для ютуб-канала"""
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel_info = None
+        self.channel_statistics = None
+        self.url = None
+        self.sub_count = None
+        self.vid_count = None
+        self.views_count = None
+        self.get_channel_data()
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        return channel
+    def get_channel_data(self) -> None:
+        """Получает информацию о канале и его статистику через API."""
+        channel_data = youtube.channels().list(
+            id=self.channel_id,
+            part='snippet,statistics'
+        ).execute()
+
+        if 'items' in channel_data:
+            self.channel_info = channel_data['items'][0]['snippet']
+            self.channel_statistics = channel_data['items'][0]['statistics']
+            self.url = channel_data['items'][0]['snippet']['customUrl']
+            self.sub_count = channel_data['items'][0]['statistics']['subscriberCount']
+            self.vid_count = channel_data['items'][0]['statistics']['videoCount']
+            self.views_count = channel_data['items'][0]['statistics']['viewCount']
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API."""
+        return youtube
+
+    def to_json(self, file_path: str) -> None:
+        """Сохраняет значения атрибутов экземпляра Channel в файл в формате JSON."""
+        data = {
+            'channel_id': self.channel_id,
+            'channel_info': self.channel_info,
+            'channel_statistics': self.channel_statistics
+        }
+
+        with open(file_path, 'w') as file:
+            json.dump(data, file)
+
+
